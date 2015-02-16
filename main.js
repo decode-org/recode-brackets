d96030cf5e304e496fcbf14b06c626019adeb9ea
Christian Silver <christian@decode.org.nz>
2015-02-16 21:25:21 +1300

    Make useless menu item have "Recode" text

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, window */
 
-/** Simple extension that adds a "File > Hello World" menu item */
+/** Extension that starts/stops Recode and saves when appropriate */
 define(function (require, exports, module) {
     "use strict";
 
     var CommandManager = brackets.getModule("command/CommandManager"),
         Menus          = brackets.getModule("command/Menus");
 
+    var recoding = false;
 
     // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        window.alert("Hello, world!");
+    function handleRecode() {
+        recoding = !recoding;
+        if (recoding) {
+            window.alert("Recode Start!");
+            command.setName("Stop Recoding");
+        } else {
+            window.alert("Recode Stop!");
+            command.setName("Recode");
+        }
     }
 
 
     // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World", MY_COMMAND_ID, handleHelloWorld);
+    var RECODE_COMMAND_ID = "recode.recode";   // package-style naming to avoid collisions
+    var command = CommandManager.register("Recode", RECODE_COMMAND_ID, handleRecode);
 
     // Then create a menu item bound to the command
     // The label of the menu item is the name we gave the command (see above)
     var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
+    menu.addMenuItem(RECODE_COMMAND_ID);
 
     // We could also add a key binding at the same time:
     //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-H");
