3078ff1ecf39d478e0ab0667239ab6a2419cbb0a
Christian Silver <christian@decode.org.nz>
2015-02-17 08:50:02 +1300

    Add simple event listening

--- main.js
+++ main.js
@@ -5,23 +5,63 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
+    var CommandManager = brackets.getModule('command/CommandManager'),
+        Menus = brackets.getModule('command/Menus'),
+        MainViewManager = brackets.getModule('view/MainViewManager'),
+        DocumentManager = brackets.getModule('document/DocumentManager');
 
-    var recoding = false;
+    var Recoder = {
+        onFileChange: function(e, newFile, newPaneId, oldFile, oldPaneId) {
+            Recoder.removeDocument();
+            Recoder.addDocument();
+        },
+        onTextChange: function(e, document, changelist) {
+            console.log(changelist);
+        },
+        addDocument: function() {
+            Recoder.currentDocument = DocumentManager.getCurrentDocument();
+            if (Recoder.currentDocument) {
+                Recoder.currentDocument.addRef();
+                Recoder.currentDocument.on('change', Recoder.onTextChange);
+            }
+        },
+        removeDocument: function() {
+            if (Recoder.currentDocument) {
+                Recoder.currentDocument.off('change', Recoder.onTextChange);
+                Recoder.currentDocument.releaseRef();
+            }
+            Recoder.currentDocument = null;
+        },
+        currentDocument: null,
+        recording: false
+    };
 
     // Function to run when the menu item is clicked
     function handleRecode() {
-        recoding = !recoding;
-        if (recoding) {
-            window.alert("Recode Start!");
-            command.setName("Stop Recoding");
+        if (!Recoder.recording) {
+            startRecode();
+            command.setName('Stop Recoding');
         } else {
-            window.alert("Recode Stop!");
-            command.setName("Recode");
+            stopRecode();
+            command.setName('Recode');
         }
     }
 
+    function startRecode() {
+        Recoder.recording = true;
+
+        // Bind events
+        MainViewManager.on('currentFileChange', Recoder.onFileChange);
+        Recoder.addDocument();
+    }
+
+    function stopRecode() {
+        Recoder.recording = false;
+
+        // Unbind events
+        MainViewManager.off('currentFileChange', Recoder.onFileChange);
+        Recoder.removeDocument();
+    }
 
     // First, register a command - a UI-less object associating an id to a handler
     var RECODE_COMMAND_ID = "recode.recode";   // package-style naming to avoid collisions
